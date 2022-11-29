579341a28db237e3f29581e4a7c8929c9060f997
KLesiu <kubaxl222@gmail.com>
2022-11-29 17:03:08 +0100

    Finish project. Add reset game function. Add function which counts rounds. Repair getComputerChoice from pick only one choice to pick as many choice as you click

--- app.js
+++ app.js
@@ -2,27 +2,50 @@ const paper = document.querySelector(".paper");
 const rock = document.querySelector(".rock");
 const scissors = document.querySelector(".scissors");
 const score = document.querySelector(".your__score");
-
+let text = document.querySelector(".text");
+let pickAi;
+let round = 1;
 let yourScore = 0;
 let player;
 const renderScore = () => {
   score.innerText = yourScore;
+  if (round == 5 && yourScore > 0) {
+    text.innerText = "You win";
+    yourScore = 0;
+    round = 0;
+  } else if (round == 5 && yourScore < 0) {
+    text.innerText = "You lose";
+    yourScore = 0;
+    round = 0;
+  } else if (round == 5 && yourScore < 1 && yourScore > -1) {
+    text.innerText = "Draw";
+    yourScore = 0;
+    round = 0;
+  } else {
+    text.innerText = "Result:";
+  }
 };
 
 const pick = () => {
   paper.addEventListener("click", () => {
     player = "Paper";
+    gameAI();
     result();
+    round++;
     return player;
   });
   rock.addEventListener("click", () => {
     player = "Rock";
+    gameAI();
     result();
+    round++;
     return player;
   });
   scissors.addEventListener("click", () => {
     player = "Scissors";
+    gameAI();
     result();
+    round++;
     return player;
   });
 };
@@ -34,7 +57,9 @@ const getComputerChoice = () => {
   // console.log(array[choose]);
   return array[choose];
 };
-let pickAi = getComputerChoice();
+const gameAI = () => {
+  pickAi = getComputerChoice();
+};
 
 const result = () => {
   if (
@@ -45,17 +70,19 @@ const result = () => {
     console.log("You lost");
     yourScore--;
     console.log(yourScore);
+    renderScore();
   } else if (player === pickAi) {
     console.log("Draw");
     console.log(yourScore);
+    renderScore();
   } else {
     console.log("You win");
     yourScore++;
     console.log(yourScore);
+    renderScore();
   }
   console.log(player);
   console.log(pickAi);
 };
-pick();
 
-renderScore();
+pick();
